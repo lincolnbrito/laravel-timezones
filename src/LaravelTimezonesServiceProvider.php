eeb86a75acf06506eba3baa2f5e916515c0ef29d
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-08 15:59:13 -0300

    criado service provider

---
+++ src/LaravelTimezonesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LincolnBrito\LaravelTimezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTimezonesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
